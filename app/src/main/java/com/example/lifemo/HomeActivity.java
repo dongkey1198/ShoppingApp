724065563e26bc8ec151ae8d1b13f5cbf904fb71
DongHyeonKim <ddk94@naver.com>
2019-09-18 15:23:53 +0800

    (2019-09-18)

---
+++ app/src/main/java/com/example/lifemo/HomeActivity.java
@@ -0,0 +1,33 @@
+package com.example.lifemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import io.paperdb.Paper;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Button LogoutBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        LogoutBtn = (Button) findViewById(R.id.logout);
+
+        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Paper.book().destroy();
+
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/lifemo/Prevelent/Prevalent.java
@@ -0,0 +1,12 @@
+package com.example.lifemo.Prevelent;
+
+import com.example.lifemo.Model.Users;
+
+public class Prevalent {
+
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+
+}
